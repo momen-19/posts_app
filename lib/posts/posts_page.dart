a7b13b53d8be1a7f2382392d972d66421e1416f3
Mohammad Alotol <m.alotol@unitone.co>
2022-08-29 15:02:01 +0300

    difference between watch, read, and listen

--- lib/posts/posts_page.dart
+++ lib/posts/posts_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:posts_app/posts/posts_controller.dart';
 
+final nameProvider = StateProvider<String>((ref) => '');
+
 class PostsPage extends ConsumerWidget {
   const PostsPage({
     Key? key,
@@ -10,9 +12,21 @@ class PostsPage extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final state = ref.watch(posts);
+    ref.listen<String>(nameProvider, (previous, next) { if(next != previous) {
+      final snackBar = SnackBar(content: Text(next));
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+    }});
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Posts'),
+        title: const NameWidget(),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              ref.read(nameProvider.state).state = 'Momen';
+            },
+          ),
+        ],
       ),
       body: state.when(
         loading: () => const Center(
@@ -29,5 +43,17 @@ class PostsPage extends ConsumerWidget {
         ),
       ),
     );
+  }
+}
+
+class NameWidget extends ConsumerWidget {
+  const NameWidget({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final name = ref.watch(nameProvider);
+    return Visibility(visible: name != '', child: Text(name));
   }
 }
